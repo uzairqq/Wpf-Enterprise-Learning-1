3e38081a459be94afc839e9c159c842866d55f7f
uzairqq <uzair.qq@outlook.com>
2017-11-20 19:05:26 +0500

    Element yani koi cheez ya tool ko kis trha dusri cheez pr bind karna jaise slider ko text box ya textblock se bind krna

--- Wpf_Learning_First/Wpf_Learning_First/MainWindow.xaml.cs
+++ Wpf_Learning_First/Wpf_Learning_First/MainWindow.xaml.cs
@@ -20,25 +20,12 @@ namespace Wpf_Learning_First
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Student stu;
+       
         public MainWindow()
         {
             InitializeComponent();
-            stu = new Student()
-                {
-                    StudentId = 3378,
-                    StudentName = "Uzair",
-                    RollNo = 155
-                };
-            DataContext = stu;
-
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            stu.studentId = 059;
-            stu.studentName = "Laraib";
-            stu.rollNo = 123123;
-        }
+      
     }
 }
